f4d36deaaf511b8b342291c08b983ff6b302c600
Leandro Rocha Pereira <lrp.0689@gmail.com>
2022-03-16 15:22:00 -0300

    Exercicio 07

---
+++ ex07.py
@@ -0,0 +1,5 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+n1 = float(input('Insira a primeira nota: '))
+n2 = float(input('Insira a segunda nota: '))
+
+print('A sua média é {}.' .format((n1 + n2)/2))
