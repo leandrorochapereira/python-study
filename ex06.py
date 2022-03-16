cbd543447f703aded7518e1d962e1dd35a2d8bd1
Leandro Rocha Pereira <lrp.0689@gmail.com>
2022-03-16 15:15:56 -0300

    Exercicio 06

--- ex06.py
+++ ex06.py
@@ -1,5 +1,4 @@
 #Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
-
 n = int(input('Digite um número: '))
 
 print('Analisando o valor {}, seu dobro é {}, seu triplo é {} e sua raiz quadrada é {:.2}.' .format(n, (n*2), (n*3), (n**(1/2))))
