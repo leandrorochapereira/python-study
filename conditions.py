c489e9e7e0c22353c64204eff6336326afc590bb
Leandro Rocha Pereira <lrp.0689@gmail.com>
2022-03-25 02:15:41 -0300

    Conditions ex 29+

---
+++ conditions.py
@@ -0,0 +1,16 @@
+'''
+aula 10 - condições
+
+tempo = int(input('Quanto tempo tem seu carro? '))
+if tempo <= 3:
+    print('Seu carro é novo!')
+else:
+    print('Seu carro é velho :(')
+print('--- FIM ---')
+
+ou podemos fazer simplificando \/
+
+tempo = int(input('Quanto tempo tem seu carro? '))
+print('Seu carro é novo :D' if tempo <=4 else 'Seu carro é velho :(')
+print('--- FIM ---')
+'''
---
+++ ex29.py
@@ -0,0 +1,7 @@
+num = int(input('Estou pensando em um número de 0 a 5, qual é? '))
+if num == 2:
+    print('Você acertou!')
+else:
+    print('Você errou! TENTE DE NOVO! ')
+print('Outra vez?!')
+
