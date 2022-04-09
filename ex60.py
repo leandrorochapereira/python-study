850b6115d281b13ede0b2a11f5f3b5c8214b4a38
Leandro Rocha Pereira <lrp.0689@gmail.com>
2022-04-08 22:36:37 -0300

    Exercises 60 and 61

---
+++ ex60.py
@@ -0,0 +1,19 @@
+'''Faça um programa que leia um número qualquer e mostre o seu fatorial. Exemplo:
+5! = 5 x 4 x 3 x 2 x 1 = 120
+
+from math import factorial
+
+n = int(input('Insira um número para calcular seu fatorial: '))
+f = factorial(n)
+print('O fatorial de {} é {}'.format(n, f))
+'''
+
+n = int(input('Insira um número para calcular seu fatorial: '))
+c = n
+f = 1
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print('{}'.format(f))
